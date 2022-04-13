17a2a4a148b316a383fa458f7bc16382f86e22df
Samueelx <gsammygitau@gmail.com>
2022-04-13 13:16:27 +0300

    Initial commit!

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pimo</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div id="heading">
+                <h2>Pimo Restaurant</h2>
+            </div>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Login</a>
+                </li>
+                <li>
+                    <a href="#">Sign Up</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section>
+            <div>
+                <h3>Place your order and have it delivered!!</h3>
+            </div>
+            <div>
+                <h4>Order your food</h4>
+                <p>
+                    Hello and welcome to your go to restaurant whenever you want the 
+                    finest meals coupled with best services. Our state-of-the-art 
+                    facilities and world class chefs are here to give you nothing but the best.
+                    Feel free to place your order!
+                </p>
+            </div>
+            <div>
+                <h4>Delivery or Pick-up</h4>
+                <p>
+                    We are flexible. Whenever you are at home or at your place of work, you 
+                    can always place your order and have your meals delivered right where you are.
+                    You van also pick your meals right here at the restaurant.
+                </p>
+            </div>
+            <div>
+                <h4>Visit Us</h4>
+                <p>
+                    Feel free to visit us and enjoy our meals and services.
+                </p>
+            </div>
+        </section>
+        
+        <section>
+
+        </section>
+
+        <section>
+
+        </section>
+
+    </main>
+
+    <footer>
+
+    </footer>
+    
+</body>
+</html>

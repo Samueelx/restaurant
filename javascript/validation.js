810644f3f0b4dbb8a7d91aefda7b1ce596bfe876
Samueelx <gsammygitau@gmail.com>
2022-04-20 15:45:33 +0300

    add validation functions for some fields.

--- javascript/validation.js
+++ javascript/validation.js
@@ -3,7 +3,8 @@ const lnameElement = document.querySelector('#lastname');
 const usernameElement = document.querySelector('#username');
 const phoneElement = document.querySelector('#phone');
 const passwordElement = document.querySelector('#password');
-const streetElement = document.querySelector('#street');
+const emailElement = document.querySelector('#email');
+//const streetElement = document.querySelector('#street');
 const townElement = document.queryElement('#town');
 
 const signup = document.querySelector('#signup');
@@ -16,7 +17,7 @@ signup.addEventListener('submit', (e) => {
 /** Returns true if the input argument is empty. */
 const isRequired = value => value === '' ? false : true;
 
-const isBetween = (length, min, max) => length < min || lenth > max ? false : true;
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
 
 /**
  * A function to check whether the email is valid. We use regular expressions here: 
@@ -30,3 +31,78 @@ const passwordCheck = (password) => {
     const rep = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})");
     return rep.test(password);
 }
+
+/**
+ * Highlights the border of the input field and displays an error message
+ */
+const showError = (input, message) => {
+    const formField = input.parentElement;
+    formField.classList.remove('success');
+    formField.classList.add('error');
+
+    const error = formField.querySelector('small');
+    error.textContent = message;
+
+    /**
+     * We're probably going to change the binding 'formField' to 'textField' 
+     * since the div parent element is of class 'txt_field' on my html files.
+     */
+}
+
+/**
+ * Shows the success indicator.
+ */
+const showSuccess = (input) => {
+    const formField = input.parentElement;
+    formField.classList.remove('error');
+    formField.classList.add('success');
+
+    const error = formField.querySelector('small')
+    error.textContent = '';
+}
+
+/**
+ * validate username field.
+ */
+const checkUsername = () => {
+    let valid = false;
+    const min = 3;
+    const max = 25;
+    const username = usernameElement.value.trim();
+
+    if(!isRequired(username)){
+        showError(usernameElement, `Username cannot be blank.`);
+    } else if(!isBetween(username.length, min, max)){
+        showError(usernameElement, `Username must be between ${min} and ${max} characters.`);
+    } else {
+        showSuccess(usernameElement);
+        valid = true;
+    }
+
+    return valid;
+}
+
+/**
+ * Validate the email field.
+ */
+
+const checkEmail = () => {
+    let valid = false;
+    const email = emailElement.value.trim();
+
+    if(!isRequired(email)){
+        showError(emailElement, 'Username cannot be blank.');
+    } else if(!validEmail(email)){
+        showError(emailElement, 'Email is not valid.');
+    }else {
+        showSuccess(emailElement)
+        valid = true;
+    }
+
+    return valid;
+}
+
+/**
+ * Validate password field.
+ */
+
--- signup.php
+++ signup.php
@@ -16,46 +16,53 @@
         <div class="center">
             <h1>Signup</h1>
             <form action="" method="post" id="signup" class="form">
-                <div class="txt_field">
+                <div class="txt_field error">
                     <input type="text" name="firstname" id="firstname">
                     <span></span>
                     <label for="firstname">First Name</label>
+                    <small></small>
                 </div>
 
-                <div class="txt_field">
+                <div class="txt_field success">
                     <input type="text" name="lastname" id="lastname">
                     <span></span>
                     <label for="lastname">Last Name</label>
+                    <small></small>
                 </div>
 
                 <div class="txt_field">
                     <input type="text" name="username" id="username">
                     <span></span>
                     <label for="username">Username</label>
+                    <small></small>
+                </div>
+
+                <div class="txt_field">
+                    <input type="text" name="email" id="email">
+                    <span></span>
+                    <label for="email">Email</label>
+                    <small></small>
                 </div>
 
                 <div class="txt_field">
                     <input type="text" name="phone" id="phone">
                     <span></span>
                     <label for="phone">Phone</label>
+                    <small></small>
                 </div>
 
                 <div class="txt_field">
                     <input type="password" name="password" id="password">
                     <span></span>
                     <label for="password">Password</label>
-                </div>
-
-                <div class="txt_field">
-                    <input type="text" name="street" id="street">
-                    <span></span>
-                    <label for="street">Street Name</label>
+                    <small></small>
                 </div>
 
                 <div class="txt_field">
                     <input type="text" name="town" id="town">
                     <span></span>
                     <label for="town">Town</label>
+                    <small></small>
                 </div>
 
                 <input type="submit" value="Signup">

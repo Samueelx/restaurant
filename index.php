1bf547f53fb090880210abb9bf2e2265c31fb8cb
Samueelx <gsammygitau@gmail.com>
2022-05-03 12:44:30 +0300

    fix some bugs.

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="./css/style.css">
+
+    <link rel="shortcut icon" href="./resources/icons/codechef.svg" type="image/x-icon">
     <title>Pimo</title>
 </head>
+
 <body>
     <header>
-        <nav>
-            <div id="heading">
-                <h2>Online Food Ordering</h2>
+        <nav class="navigation">
+            <div>
+                <h2 id="heading">Online Food Ordering</h2>
             </div>
             <ul id="navigation-list" class="navigation-list">
                 <li class="nav-item">
@@ -26,7 +33,7 @@
             </ul>
         </nav>
     </header>
-
+    <!--
     <main>
         <section>
             <div>
@@ -35,8 +42,8 @@
             <div>
                 <h4>Order your food</h4>
                 <p>
-                    Hello and welcome to your go to restaurant whenever you want the 
-                    finest meals coupled with best services. Our state-of-the-art 
+                    Hello and welcome to your go to restaurant whenever you want the
+                    finest meals coupled with best services. Our state-of-the-art
                     facilities and world class chefs are here to give you nothing but the best.
                     Feel free to place your order!
                 </p>
@@ -44,7 +51,7 @@
             <div>
                 <h4>Delivery or Pick-up</h4>
                 <p>
-                    We are flexible. Whenever you are at home or at your place of work, you 
+                    We are flexible. Whenever you are at home or at your place of work, you
                     can always place your order and have your meals delivered right where you are.
                     You van also pick your meals right here at the restaurant.
                 </p>
@@ -56,20 +63,21 @@
                 </p>
             </div>
         </section>
-        
-        <section>
-
-        </section>
 
         <section>
 
         </section>
 
-    </main>
+        <section>
+
+        </section>
+
+    </main> -->
 
     <footer>
 
     </footer>
-    
+
 </body>
+
 </html>

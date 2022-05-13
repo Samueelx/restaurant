f9b9a09063e54c5c957f669d3c6f3781593c45ff
Samueelx <gsammygitau@gmail.com>
2022-05-13 11:03:39 +0300

    add a few more pages for the admin.

---
+++ admin/manage-category.php
@@ -0,0 +1,58 @@
+<?php
+include('./partials/menu.php');
+?>
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Manage Category</h1>
+
+        <br /> <br />
+
+        <!--Button to add admin-->
+        <a href="#" class="btn-primary">Add Category</a>
+
+        <br /> <br /> <br />
+        <table class="tbl-full">
+            <tr>
+                <th>Username</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Actions</th>
+            </tr>
+
+            <tr>
+                <td>fancynancy</td>
+                <td>Fancy</td>
+                <td>Nancy</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>fancynancy</td>
+                <td>Fancy</td>
+                <td>Nancy</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>fancynancy</td>
+                <td>Fancy</td>
+                <td>Nancy</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+
+<?php
+include('./partials/footer.php');
+?>

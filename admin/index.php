4d948b0cfc51d7178f0717cb14cb68634e8fdcc5
Samueelx <gsammygitau@gmail.com>
2022-06-11 14:52:59 +0300

    show some summary data on the admin dashboard page

--- admin/index.php
+++ admin/index.php
@@ -17,27 +17,47 @@ include('./partials/menu.php');
         <br>
 
         <div class="col-4">
-            <h1>5</h1>
+            <?php
+            $categories = "SELECT COUNT(*) FROM menu_type;";
+            $res = mysqli_query($conn, $categories);
+            $cats = mysqli_fetch_assoc($res);
+            ?>
+            <h1><?php echo $cats['COUNT(*)']; ?></h1>
             <br>
             <p>Categories</p>
         </div>
 
         <div class="col-4">
-            <h1>5</h1>
+            <?php
+            $menu_items = "SELECT COUNT(*) FROM Menu;";
+            $res = mysqli_query($conn, $menu_items);
+            $items = mysqli_fetch_assoc($res);
+            ?>
+            <h1><?php echo $items['COUNT(*)']; ?></h1>
             <br>
-            <p>Categories</p>
+            <p>Food Items</p>
         </div>
 
         <div class="col-4">
-            <h1>5</h1>
+            <?php
+            $orders = "SELECT COUNT(*) FROM orders;";
+            $res = mysqli_query($conn, $orders);
+            $num_of_orders = mysqli_fetch_assoc($res);
+            ?>
+            <h1><?php echo $num_of_orders['COUNT(*)']; ?></h1>
             <br>
-            <p>Categories</p>
+            <p>Total Orders</p>
         </div>
 
         <div class="col-4">
-            <h1>5</h1>
+            <?php
+            $total = "SELECT SUM(total_amount) AS 'total' FROM orders WHERE order_status = 'dispatched';";
+            $res = mysqli_query($conn, $total);
+            $total_revenue = mysqli_fetch_assoc($res);
+            ?>
+            <h1><?php echo "KES ". $total_revenue['total']; ?></h1>
             <br>
-            <p>Categories</p>
+            <p>Revenue Generated</p>
         </div>
 
         <div class="clearfix"></div>
